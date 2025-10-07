03cbbc7620ea0b2b8c19292b98eabbbfe470fcbd
Zehab-bid <zehabidha152gmail.com>
2025-10-07 15:00:52 +0700

    end

--- pertemuan1.py
+++ pertemuan1.py
@@ -1,2 +1,13 @@
 percentage = float(input('masukkan persentase nilai siswa'))
 
+if percentage >= 90:
+    print("excelent performance")
+elif percentage >= 80:
+    print("very good performance")
+elif percentage >= 70:
+    print("goood performance")
+elif percentage >= 60:
+    print("average performance")
+else:
+    print("needs improvement")
+    
